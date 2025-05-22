704e4084e2bba510fcbd35e0442380d1d973be22
mohammed-shaneaf <mohammed.shaneaf.963@gmail.com>
2025-05-23 00:36:58 +0300

    Ending Splash And Init Login Feature

---
+++ lib/features/auth/presentation/views/login_view.dart
@@ -0,0 +1,11 @@
+import 'package:book_app_clean_arch/features/Auth/presentation/views/widgets/login_view_body.dart';
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  const LoginView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: LoginViewBody());
+  }
+}
--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,14 +1,65 @@
+import 'package:book_app_clean_arch/core/routes/app_router.dart';
+import 'package:book_app_clean_arch/core/utils/app_styles.dart';
+import 'package:book_app_clean_arch/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
 
   @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProviderStateMixin {
+  late final AnimationController animationController;
+  late final Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    animationController = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+
+    slidingAnimationInit();
+
+    animationController.forward();
+
+    navigationToLoginScreen();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose(); // Don't forget to dispose the controller
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [],
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(Assets.assetsImagesLogo),
+        Center(
+          child: SlideTransition(
+            position: slidingAnimation,
+            child: Text('Read Book For Free', style: AppStyles.textStyle18),
+          ),
+        ),
+      ],
     );
+  }
+
+  Future<void> navigationToLoginScreen() {
+    return Future.delayed(const Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, AppRouter.login);
+    });
+  }
+
+  Animation<Offset> slidingAnimationInit() {
+    return slidingAnimation = Tween<Offset>(
+      begin: const Offset(0, 2),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(parent: animationController, curve: Curves.easeOut));
   }
 }
